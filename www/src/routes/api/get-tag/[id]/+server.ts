b2f2d6f306ca2f8c0bac56e38bb7ef142f8beaed
hyper-neutrino <alex.yj.liao@gmail.com>
2023-07-26 12:04:38 -0400

    prevent /api/get-tag from polluting the output

--- www/src/routes/api/get-tag/[id]/+server.ts
+++ www/src/routes/api/get-tag/[id]/+server.ts
@@ -2,5 +2,9 @@ import { hq_bot } from "../../../../bot.js";
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ params }) => {
-    return new Response((await hq_bot.users.fetch(params.id!)).tag);
+    try {
+        return new Response((await hq_bot.users.fetch(params.id!)).tag);
+    } catch {
+        return new Response("", { status: 404 });
+    }
 };
