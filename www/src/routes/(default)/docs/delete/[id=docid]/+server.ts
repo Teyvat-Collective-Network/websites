3c273a343b97a921a5f7d0ec336c360a240dc3eb
hyper-neutrino <alex.yj.liao@gmail.com>
2023-07-26 15:17:38 -0400

    add trace for deleter and allow observers to delete anything

--- www/src/routes/(default)/docs/delete/[id=docid]/+server.ts
+++ www/src/routes/(default)/docs/delete/[id=docid]/+server.ts
@@ -1,18 +1,21 @@
 import type { RequestHandler } from "@sveltejs/kit";
 import db from "../../../../../db.js";
 
-export const POST: RequestHandler = async ({ params, request, locals }) => {
+export const POST: RequestHandler = async ({ params, locals }) => {
     try {
         if (!(locals as any).auth) throw "You are not authorized to use the TCN Documents feature.";
 
         const data = await db.docs.findOne({ id: params.id });
         if (!data) throw "This document no longer exists.";
-        if (data.author !== (locals as any).user.id)
-            throw "You may only delete your own documents.";
+        if (data.author !== (locals as any).user.id && !data.observer)
+            throw "You may only delete your own documents unless you are an observer.";
     } catch (error: any) {
         return new Response(JSON.stringify({ error }));
     }
 
-    await db.docs.findOneAndUpdate({ id: params.id }, { $set: { deleted: true } });
+    await db.docs.findOneAndUpdate(
+        { id: params.id },
+        { $set: { deleted: true, deleter: (locals as any).user.id } },
+    );
     return new Response(JSON.stringify({}));
 };
